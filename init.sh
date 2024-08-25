9c4974c07a0c923750b708bcaec6090b5da76188
Chris Song <sjhshy@gmail.com>
2024-08-26 06:08:08 +0900

    add deps

---
+++ init.sh
@@ -0,0 +1,15 @@
+
+# pyenv
+curl https://pyenv.run | bash
+
+# nvm
+
+sudo apt-get update
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# install python
+pyenv install 3.9.19
+
+# install node v20
+nvm install v20.0.0
+
