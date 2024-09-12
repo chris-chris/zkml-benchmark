5894fc164ea7a8fda859bc7c8036ec6a11f46a36
Ubuntu <ubuntu@ip-172-31-92-239.ec2.internal>
2024-09-12 01:29:26 +0000

    kimchi exp by depth

--- linux_bench.sh
+++ linux_bench.sh
@@ -4,7 +4,7 @@
 mkdir -p logs
 
 # CSV 파일 초기화
-output_csv="experiment_results.csv"
+output_csv="experiment_kimchi.csv"
 echo "framework,model,exp_num,proving_time,memory_usage,cpu_usage" > $output_csv
 
 # 함수: 성능 데이터를 추출하는 기능
@@ -47,8 +47,9 @@ for i in $(seq 1 15)
 # for i in 12 13 14 15 16 17 18 19 20 21 22 23 24 25 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 # for ((i = 1; i <= 2; i++)); do
-    run_benchmark "ezkl" $i "ezkl prove --witness models/mlp/mlp$i/witness.json --pk-path models/mlp/mlp$i/pk.key --compiled-circuit models/mlp/mlp$i/model.compiled --proof-path models/mlp/mlp$i/proof.json"
-    run_benchmark "o1js" $i "node dist/mlp.js $i"
+    # run_benchmark "ezkl" $i "ezkl prove --witness models/mlp/mlp$i/witness.json --pk-path models/mlp/mlp$i/pk.key --compiled-circuit models/mlp/mlp$i/model.compiled --proof-path models/mlp/mlp$i/proof.json"
+    # run_benchmark "o1js" $i "node dist/mlp.js $i"
+    run_benchmark "kimchi" $i "cargo run --manifest-path /home/ubuntu/zkml-benchmark/kimchi_exp/Cargo.toml $i"
     # run_benchmark "orion" $i "scarb run --path models/linear_regression/orion"
     # run_benchmark "orion" $i "jupyter nbconvert --to notebook --execute ./models/mlp/orion/orion.ipynb --output orion_output"
     # run_benchmark "risczero" $i "./zkvm"
